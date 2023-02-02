bb525abb1835d61aa44c2ba6dd977cdecff5cfed
sampaixd <samuelbroman03@gmail.com>
2023-02-02 10:36:38 +0100

    implemented dynamic lists

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 
+#define DEV_MODE // comment out to remove dev UI
 typedef struct shape_t
 {
     Vector2 position; // center in circle, upper left corner for rects
@@ -25,13 +26,15 @@ typedef struct rect_t
     Vector2 size;
 } rect_t;
 
-typedef struct balls_list_t {
+typedef struct balls_list_t
+{
     ball_t *balls;
     int max;
     int pointer;
 } balls_list_t;
 
-typedef struct rects_list_t {
+typedef struct rects_list_t
+{
     rect_t *rects;
     int max;
     int pointer;
@@ -40,10 +43,13 @@ typedef struct rects_list_t {
 const int screenWidth = 1280;
 const int screenHeight = 720;
 const int targetFPS = 60;
+const int listStartMax = 16;
+
 const float deltaFrameTime = 1 / (float)targetFPS;
 const float gravity = 9.82;
 const float mapBoundraryCollisionBouce = 0.2; // 1 means no force is lost upon wall collision, everything above 1 will cause a increase in force for every collision
-
+// 1000 units tolerance for the velocity, if it goes beyound that it's possible for the shapes to go out of bounds
+// NOTE! the corners don't have this tolerance as of now, might fix that later
 const Rectangle mapBoundraryUpper = {0, -1000, screenWidth, 1000};
 const Rectangle mapBoundraryLower = {0, screenHeight, screenWidth, 1000};
 const Rectangle mapBoundraryLeft = {-1000, 0, 1000, screenHeight};
@@ -61,9 +67,34 @@ void IncreaseBallsListSize(balls_list_t *balls)
     balls->balls = realloc(balls->balls, sizeof(ball_t) * balls->max);
 }
 
-void IncreaseRectsListSize(rects_list_t *rects) {
+void HandleAddingBallToList(balls_list_t *balls, ball_t newBall)
+{
+    if (balls->pointer >= balls->max)
+    {
+        IncreaseBallsListSize(balls);
+    }
+    AddBall(balls, newBall);
+}
+
+void AddRect(rects_list_t *rects, rect_t newRect)
+{
+    rects->rects[rects->pointer] = newRect;
+    rects->pointer += 1;
+}
+
+void IncreaseRectsListSize(rects_list_t *rects)
+{
     rects->max *= 2;
     rects->rects = realloc(rects->rects, sizeof(rect_t) * rects->max);
+}
+
+void HandleAddingRectToList(rects_list_t *rects, rect_t newRect)
+{
+    if (rects->pointer >= rects->max)
+    {
+        IncreaseRectsListSize(rects);
+    }
+    AddRect(rects, newRect);
 }
 
 Rectangle GetRectangleFromRect_t(rect_t rect)
@@ -81,47 +112,49 @@ void MoveShapeBasedOnMousePosition(Vector2 *shapePos, Vector2 *shapeVelocity, Ve
 {
     float posX = GetMousePosition().x + mouseShapeOffset.x;
     float posY = GetMousePosition().y + mouseShapeOffset.y;
+#ifdef DEV_MODE
     printf("new posX: %.2f - new posY: %.2f\n"
            "shapePosX: %.2f - shapePosY: %.2f\n",
            posX, posY,
            shapePos->x, shapePos->y);
     printf("velY: %.2f, velX: %.2f\n",
            shapeVelocity->y, shapeVelocity->x);
+#endif
     shapeVelocity->x = (posX - shapePos->x);
     shapeVelocity->y = (posY - shapePos->y);
 }
 
 // grabs shape with mouse
-int GrabShape(Vector2 mousePos, ball_t *balls, int ballsCount, rect_t *rects, int rectsCount, Vector2 *mouseShapeOffset)
+int GrabShape(Vector2 mousePos, balls_list_t *balls,rects_list_t *rects, Vector2 *mouseShapeOffset)
 {
-    for (int i = 0; i < ballsCount; i++)
+    for (int i = 0; i < balls->pointer; i++)
     {
-        if (CheckCollisionPointCircle(mousePos, balls[i].base.position, balls[i].radius))
+        if (CheckCollisionPointCircle(mousePos, balls->balls[i].base.position, balls->balls[i].radius))
         {
-            SetMouseShapeOffset(mouseShapeOffset, mousePos, balls[i].base.position);
+            SetMouseShapeOffset(mouseShapeOffset, mousePos, balls->balls[i].base.position);
             return i;
         }
     }
-    for (int i = 0; i < rectsCount; i++)
+    for (int i = 0; i < rects->pointer; i++)
     {
-        if (CheckCollisionPointRec(mousePos, GetRectangleFromRect_t(rects[i])))
+        if (CheckCollisionPointRec(mousePos, GetRectangleFromRect_t(rects->rects[i])))
         {
-            SetMouseShapeOffset(mouseShapeOffset, mousePos, rects[i].base.position);
-            return i + ballsCount;
+            SetMouseShapeOffset(mouseShapeOffset, mousePos, rects->rects[i].base.position);
+            return i + balls->pointer;
         }
     }
     return -1;
 }
 
-void Gravity(ball_t *balls, int ballsCount, rect_t *rects, int rectsCount)
+void Gravity(balls_list_t *balls, rects_list_t *rects)
 {
-    for (int i = 0; i < ballsCount; i++)
+    for (int i = 0; i < balls->pointer; i++)
     {
-        balls[i].base.velocity.y += gravity * deltaFrameTime;
+        balls->balls[i].base.velocity.y += gravity * deltaFrameTime;
     }
-    for (int i = 0; i < rectsCount; i++)
+    for (int i = 0; i < rects->pointer; i++)
     {
-        rects[i].base.velocity.y += gravity * deltaFrameTime;
+        rects->rects[i].base.velocity.y += gravity * deltaFrameTime;
     }
 }
 
@@ -131,47 +164,55 @@ void MoveShapeWithVelocity(Vector2 *position, Vector2 velocity)
     position->y += velocity.y;
 }
 
-void MoveShapes(ball_t *balls, int ballsCount, rect_t *rects, int rectsCount)
+void MoveShapes(balls_list_t *balls, rects_list_t *rects)
 {
-    for (int i = 0; i < ballsCount; i++)
-    {
         // TODO add spinning of shapes
-        MoveShapeWithVelocity(&balls[i].base.position, balls[i].base.velocity);
-    }
-    for (int i = 0; i < rectsCount; i++)
+    for (int i = 0; i < balls->pointer; i++)
     {
-        MoveShapeWithVelocity(&rects[i].base.position, rects[i].base.velocity);
+        MoveShapeWithVelocity(&balls->balls[i].base.position, balls->balls[i].base.velocity);
+    }
+    for (int i = 0; i < rects->pointer; i++)
+    {
+        MoveShapeWithVelocity(&rects->rects[i].base.position, rects->rects[i].base.velocity);
     }
 }
 
-void HandleMapWallCollision(ball_t *balls, int ballsCount, rect_t *rects, int rectsCount)
+void HandleMapWallCollision(balls_list_t *balls, rects_list_t *rects)
 {
-    for (int i = 0; i < ballsCount; i++)
+    for (int i = 0; i < balls->pointer; i++)
     {
-        if (CheckCollisionCircleRec(balls[i].base.position, balls[i].radius, mapBoundraryUpper))
+        if (CheckCollisionCircleRec(balls->balls[i].base.position, balls->balls[i].radius, mapBoundraryUpper))
         {
-            balls[i].base.velocity.y *= -mapBoundraryCollisionBouce;
-            balls[i].base.position.y = 0 + balls[i].radius; // makes sure that the circle cannot go inside the map boundrary
+            balls->balls[i].base.velocity.y *= -mapBoundraryCollisionBouce;
+            balls->balls[i].base.position.y = 0 + balls->balls[i].radius; // makes sure that the circle cannot go inside the map boundrary
         }
-        else if (CheckCollisionCircleRec(balls[i].base.position, balls[i].radius, mapBoundraryLower))
+        else if (CheckCollisionCircleRec(balls->balls[i].base.position, balls->balls[i].radius, mapBoundraryLower))
         {
-            balls[i].base.velocity.y *= -mapBoundraryCollisionBouce;
-            balls[i].base.position.y = screenHeight - balls[i].radius; // makes sure that the circle cannot go inside the map boundrary
+            balls->balls[i].base.velocity.y *= -mapBoundraryCollisionBouce;
+            balls->balls[i].base.position.y = screenHeight - balls->balls[i].radius; // makes sure that the circle cannot go inside the map boundrary
         }
-        if (CheckCollisionCircleRec(balls[i].base.position, balls[i].radius, mapBoundraryLeft))
+        if (CheckCollisionCircleRec(balls->balls[i].base.position, balls->balls[i].radius, mapBoundraryLeft))
         {
-            balls[i].base.velocity.x *= -mapBoundraryCollisionBouce;
-            balls[i].base.position.x = 0 + balls[i].radius; // makes sure that the circle cannot go inside the map boundrary
+            balls->balls[i].base.velocity.x *= -mapBoundraryCollisionBouce;
+            balls->balls[i].base.position.x = 0 + balls->balls[i].radius; // makes sure that the circle cannot go inside the map boundrary
         }
-        else if (CheckCollisionCircleRec(balls[i].base.position, balls[i].radius, mapBoundraryRight))
+        else if (CheckCollisionCircleRec(balls->balls[i].base.position, balls->balls[i].radius, mapBoundraryRight))
         {
-            balls[i].base.velocity.x *= -mapBoundraryCollisionBouce;
-            balls[i].base.position.x = screenWidth - balls[i].radius; // makes sure that the circle cannot go inside the map boundrary
+            balls->balls[i].base.velocity.x *= -mapBoundraryCollisionBouce;
+            balls->balls[i].base.position.x = screenWidth - balls->balls[i].radius; // makes sure that the circle cannot go inside the map boundrary
         }
     }
-    for (int i = 0; i < rectsCount; i++)
+    for (int i = 0; i < rects->pointer; i++)
     {
         // add later
+    }
+}
+
+void DrawBalls(balls_list_t balls)
+{
+    for (int i = 0; i < balls.pointer; i++)
+    {
+        DrawCircleV(balls.balls[i].base.position, balls.balls[i].radius, RED);
     }
 }
 
@@ -183,48 +224,50 @@ int main()
     // balls have their regular index, rects have their index + ballsCount
     int selectedShape = -1;
     Vector2 mouseShapeOffset = {0, 0};
-    ball_t balls[1] = {};
-    int ballsCount = 1;
-    rect_t rects[1] = {};
-    int rectsCount = 0;
-    balls[0] = (ball_t){(shape_t){(Vector2){screenWidth / 2, screenHeight / 2}, 10, 0, (Vector2){0, 0}, 0, false}, 10};
+    balls_list_t balls = {malloc(sizeof(ball_t) * listStartMax), listStartMax, 0};
+    rects_list_t rects = {malloc(sizeof(rect_t) * listStartMax), listStartMax, 0};
+
+    HandleAddingBallToList(&balls, (ball_t){(shape_t){(Vector2){screenWidth / 2, screenHeight / 2}, 10, 0, (Vector2){0, 0}, 0, false}, 10});
+
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(LIGHTGRAY);
         if (IsKeyDown(KEY_A) && selectedShape == -1)
         {
-            selectedShape = GrabShape(GetMousePosition(), balls, ballsCount, rects, rectsCount, &mouseShapeOffset);
+            selectedShape = GrabShape(GetMousePosition(), &balls, &rects, &mouseShapeOffset);
         }
         if (IsKeyReleased(KEY_A))
         {
             selectedShape = -1;
         }
-        DrawCircleV(balls[0].base.position, balls[0].radius, RED);
-        Gravity(balls, ballsCount, rects, rectsCount);
-        MoveShapes(balls, ballsCount, rects, rectsCount);
-        HandleMapWallCollision(balls, ballsCount, rects, rectsCount);
+        BeginDrawing();
+        ClearBackground(LIGHTGRAY);
+        DrawBalls(balls);
+        Gravity(&balls, &rects);
+        MoveShapes(&balls, &rects);
+        HandleMapWallCollision(&balls, &rects);
         if (selectedShape != -1)
         {
-            if (selectedShape > ballsCount - 1)
+            if (selectedShape > balls.pointer - 1)
             {
-                MoveShapeBasedOnMousePosition(&rects[selectedShape - ballsCount].base.position, &rects[selectedShape - ballsCount].base.velocity, mouseShapeOffset);
+                MoveShapeBasedOnMousePosition(&rects.rects[selectedShape - balls.pointer].base.position, &rects.rects[selectedShape - balls.pointer].base.velocity, mouseShapeOffset);
             }
             else
             {
-                MoveShapeBasedOnMousePosition(&balls[selectedShape].base.position, &balls[selectedShape].base.velocity, mouseShapeOffset);
+                MoveShapeBasedOnMousePosition(&balls.balls[selectedShape].base.position, &balls.balls[selectedShape].base.velocity, mouseShapeOffset);
             }
         }
+#ifdef DEV_MODE
         DrawText(TextFormat("ball 1 data\n"
                             "position x:y - %.2f:%.2f\n"
                             "velocity x:y - %.2f:%.2f\n"
                             "current shape grabbed: %d\n"
                             "is holding left mouse button: %d",
-                            balls[0].base.position.x, balls[0].base.position.y,
-                            balls[0].base.velocity.x, balls[0].base.velocity.y,
+                            balls.balls[0].base.position.x, balls.balls[0].base.position.y,
+                            balls.balls[0].base.velocity.x, balls.balls[0].base.velocity.y,
                             selectedShape, IsKeyDown(KEY_A)),
                  20, 20, 20, DARKGREEN);
+#endif
         EndDrawing();
     }
     return 0;
